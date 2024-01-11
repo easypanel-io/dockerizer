8acf776a3f69346122f0a7cfe820bcf102c7e3e0
serban-alexandru <alexserb2009@yahoo.com>
2024-01-11 15:23:51 +0200

    test laravel

--- src/dockerizers/laravel/index.ts
+++ src/dockerizers/laravel/index.ts
@@ -4,13 +4,21 @@ import { z } from "zod";
 import files from "./files.json";
 
 export const schema = z.object({
-  php_version: z.string(),
-  packages: z.string(),
-  php_extensions: z.string(),
-  port: z.number(),
+  php_version: z.string().default("7.4-apache"),
+  packages: z
+    .string()
+    .default(
+      "libzip-dev unzip libpng-dev libjpeg-dev libonig-dev libxml2-dev curl"
+    ),
+  php_extensions: z.string().default("pdo_mysql zip exif pcntl bcmath gd"),
+  port: z.number().default(80),
 });
 
-export function generate(input: z.infer<typeof schema>): Files {
+export const defaultValues = schema.parse({});
+
+export function generate(rawInput: z.infer<typeof schema>): Files {
+  const input = schema.parse(rawInput);
+
   return {
     ...files,
     Dockerfile: renderString(files["Dockerfile"], input),
