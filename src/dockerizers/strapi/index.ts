3cc667d41b9a851172472aad9b3092a20750a395
serban-alexandru <alexserb2009@yahoo.com>
2024-01-11 15:57:40 +0200

    strapi test

--- src/dockerizers/strapi/index.ts
+++ src/dockerizers/strapi/index.ts
@@ -4,17 +4,24 @@ import { z } from "zod";
 import files from "./files.json";
 
 export const schema = z.object({
-  node_version: z.string(),
-  alpine: z.boolean(),
-  alpinePackages: z.string(),
-  node_env: z.string(),
-  build_packages: z.string(),
-  production_packages: z.string(),
-  user: z.string(),
-  port: z.string(),
+  node_version: z.string().default("node:18"),
+  alpine: z.boolean().default(true),
+  alpinePackages: z
+    .string()
+    .default(
+      "build-base gcc autoconf automake zlib-dev libpng-dev vips-dev git"
+    ),
+  node_env: z.string().default("production"),
+  build_packages: z.string().default("node-gyp"),
+  production_packages: z.string().default("vips-dev"),
+  user: z.string().default("node"),
+  port: z.string().default("1337"),
 });
 
-export function generate(input: z.infer<typeof schema>): Files {
+export const defaultValues = schema.parse({});
+
+export function generate(rawInput: z.infer<typeof schema>): Files {
+  const input = schema.parse(rawInput);
   return {
     ...files,
     Dockerfile: renderString(files["Dockerfile"], input),
