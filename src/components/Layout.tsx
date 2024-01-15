0e533b50ef6ca042c8505f381538dab856d6ff4e
Andrei Canta <deiucanta@gmail.com>
2024-01-15 12:52:55 +0100

    update layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
 import { Toaster } from "@/components/ui/sonner";
 import { downloadZip } from "@/lib/download";
 import { detectLanguage } from "@/lib/highlight";
 import { Files } from "@/lib/types";
-import { CopyIcon, DownloadIcon, ShipIcon } from "lucide-react";
+import { debounce } from "lodash";
+import { CopyIcon, DownloadIcon } from "lucide-react";
 import { Inter } from "next/font/google";
 import Link from "next/link";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useCallback, useEffect, useState } from "react";
+import { UseFormReturn } from "react-hook-form";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 import { toast } from "sonner";
@@ -16,7 +19,7 @@ const inter = Inter({ subsets: ["latin"] });
 export function Layout({ children }: { children: ReactNode }) {
   return (
     <main className={`${inter.className}`}>
-      <div className="fixed left-0 right-0 top-0 z-10 flex justify-between bg-slate-300 mb-10 p-4">
+      <div className="fixed left-0 right-0 top-0 z-10 flex justify-between bg-gray-300 mb-10 p-4">
         <div className="">
           <Link href="/">Dockerizer</Link> by{" "}
           <Link href="https://easypanel.io/">Easypanel</Link>
@@ -33,98 +36,83 @@ export function DockerizerLayout({
   children,
   title,
   description,
-  getFiles,
+  form,
+  generate,
 }: {
   children: ReactNode;
   title: ReactNode;
   description?: ReactNode;
-  getFiles: () => Files;
+  form: UseFormReturn<any>;
+  generate: (values: any) => Files;
 }) {
+  const getFiles = () => generate(form.getValues());
+  const download = () => downloadZip(getFiles());
+  const preview = () => setPreviewFiles(getFiles());
+  const previewDebounced = useCallback(debounce(preview, 1000), []);
+
   const [previewFiles, setPreviewFiles] = useState<Files | null>(null);
-
-  async function download() {
-    const files = getFiles();
-    await downloadZip(files);
-  }
-
-  function preview() {
-    setPreviewFiles(getFiles());
-  }
-
-  useEffect(() => {
-    if (previewFiles === null) {
-      preview();
-    }
-  }, []);
+  useEffect(preview, []);
+  form.watch(previewDebounced);
 
   return (
-    <Layout>
-      <div className="md:flex justify-between">
-        <div>
-          <h1 className="text-3xl tracking-tight font-medium">
-            Dockerize {title} Applications
-          </h1>
-          {description && <h2 className="text-xl">{description}</h2>}
+    <Form {...form}>
+      <Layout>
+        <div className="md:flex justify-between">
+          <div>
+            <h1 className="text-3xl tracking-tight font-medium">
+              Dockerize {title} Applications
+            </h1>
+            {description && <h2 className="text-xl">{description}</h2>}
+          </div>
+          <div className="space-x-2">right</div>
         </div>
-        <div className="space-x-2">
-          <Button onClick={download} variant="secondary">
-            <DownloadIcon className="mr-2 h-4 w-4" />
-            Download
-          </Button>
-          <Button onClick={preview}>
-            <ShipIcon className="mr-2 h-4 w-4" />
-            Dockerize
-          </Button>
-        </div>
-      </div>
-      <div className="grid lg:grid-cols-3 gap-8 mt-10">
-        <div className="lg:col-span-1 min-w-0">
-          <div className="space-y-6">{children}</div>
-          <div className="mt-6">
-            <div className="space-x-2">
-              <Button onClick={download} variant="secondary">
-                <DownloadIcon className="mr-2 h-4 w-4" />
-                Download
-              </Button>
-              <Button onClick={preview}>
-                <ShipIcon className="mr-2 h-4 w-4" />
-                Dockerize
-              </Button>
-            </div>
+        <div className="space-y-8 mt-10">
+          <div className="">
+            <div className="space-y-6">{children}</div>
+          </div>
+          <div>
+            <h2>Setup & Build</h2>
+            <Button onClick={download} variant="secondary">
+              <DownloadIcon className="mr-2 h-4 w-4" />
+              Download
+            </Button>
+          </div>
+          <div className="">
+            {previewFiles && Object.entries(previewFiles).length > 0 && (
+              <div className="spacy-y-4">
+                {Object.entries(previewFiles).map(([name, content], index) => (
+                  <div
+                    key={name}
+                    className="rounded-md overflow-hidden bg-gray-50 border"
+                  >
+                    <div className="flex justify-between items-center p-2 border-b">
+                      <div className="pl-2 text-sm font-medium">{name}</div>
+                      <Button
+                        size="xs"
+                        variant="outline"
+                        onClick={() => {
+                          navigator.clipboard.writeText(content);
+                          toast("Copied to clipboard");
+                        }}
+                      >
+                        <CopyIcon className="mr-2 h-3.5 w-3.5" />
+                        Copy
+                      </Button>
+                    </div>
+                    <SyntaxHighlighter
+                      language={detectLanguage(name)}
+                      style={docco}
+                      className="text-sm !bg-gray-50 !p-4"
+                    >
+                      {content}
+                    </SyntaxHighlighter>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
-        <div className="lg:col-span-2 min-w-0">
-          {previewFiles && Object.entries(previewFiles).length > 0 && (
-            <div className="spacy-y-4">
-              {Object.entries(previewFiles).map(([name, content], index) => (
-                <div key={name} className="rounded overflow-hidden">
-                  <div className="flex justify-between items-center bg-slate-200 p-2">
-                    <div className="pl-2 text-sm font-medium">{name}</div>
-                    <Button
-                      size="xs"
-                      variant="outline"
-                      onClick={() => {
-                        navigator.clipboard.writeText(content);
-                        toast("Copied to clipboard");
-                      }}
-                    >
-                      <CopyIcon className="mr-2 h-3.5 w-3.5" />
-                      Copy
-                    </Button>
-                  </div>
-                  <SyntaxHighlighter
-                    language={detectLanguage(name)}
-                    style={docco}
-                    className="text-sm !bg-slate-100 !p-4"
-                  >
-                    {content}
-                  </SyntaxHighlighter>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
-      </div>
-    </Layout>
+      </Layout>
+    </Form>
   );
 }
--- src/pages/laravel/index.tsx
+++ src/pages/laravel/index.tsx
@@ -1,5 +1,4 @@
 import { DockerizerLayout } from "@/components/Layout";
-import { Form } from "@/components/ui/form";
 import { FormInput, FormSelect } from "@/components/ui/form-fields";
 import { generate, phpVersionOptions, schema } from "@/dockerizers/laravel";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -19,44 +18,39 @@ export default function Page() {
   });
 
   return (
-    <DockerizerLayout
-      title="Laravel"
-      getFiles={() => generate(form.getValues())}
-    >
-      <Form {...form}>
-        <FormSelect
-          control={form.control}
-          name="phpVersion"
-          label="PHP version"
-          options={phpVersionOptions}
-        />
-        <FormInput
-          control={form.control}
-          name="packages"
-          label="Packages"
-          description="packages that will be installed"
-          inputProps={{
-            placeholder: "unzip curl...",
-          }}
-        />
-        <FormInput
-          control={form.control}
-          name="phpExtensions"
-          label="PHP Extensions"
-          inputProps={{
-            placeholder: "pdo_mysql zip exif...",
-          }}
-        />
-        <FormInput
-          control={form.control}
-          name="port"
-          label="Port"
-          inputProps={{
-            placeholder: "80",
-            type: "number",
-          }}
-        />
-      </Form>
+    <DockerizerLayout title="Laravel" form={form} generate={generate}>
+      <FormSelect
+        control={form.control}
+        name="phpVersion"
+        label="PHP version"
+        options={phpVersionOptions}
+      />
+      <FormInput
+        control={form.control}
+        name="packages"
+        label="Packages"
+        description="packages that will be installed"
+        inputProps={{
+          placeholder: "unzip curl...",
+        }}
+      />
+      <FormInput
+        control={form.control}
+        name="phpExtensions"
+        label="PHP Extensions"
+        inputProps={{
+          placeholder: "pdo_mysql zip exif...",
+        }}
+      />
+      <FormInput
+        control={form.control}
+        name="port"
+        label="Port"
+        inputProps={{
+          placeholder: "80",
+          type: "number",
+        }}
+      />
     </DockerizerLayout>
   );
 }
