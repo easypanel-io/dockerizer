da6ad7418ef0f0beea6090ac5928f6e6b378295d
serban-alexandru <alexserb2009@yahoo.com>
2024-01-15 16:15:30 +0200

    nestjs test

---
+++ src/dockerizers/nestjs/index.ts
@@ -0,0 +1,26 @@
+import { Files } from "@/lib/types";
+import { renderString } from "nunjucks";
+import { z } from "zod";
+import files from "./files.json";
+
+export const schema = z.object({
+  nodeImage: z.string().default("node:18"),
+});
+
+export const nodeImageOptions = [
+  "node:16",
+  "node:16-alpine",
+  "node:18",
+  "node:18-alpine",
+  "node:20",
+  "node:20-alpine",
+];
+
+export const defaultValues = schema.parse({});
+
+export function generate(rawInput: z.infer<typeof schema>): Files {
+  const input = schema.parse(rawInput);
+  return {
+    Dockerfile: renderString(files["Dockerfile.njk"], input),
+  };
+}
---
+++ src/pages/nestjs/index.tsx
@@ -0,0 +1,29 @@
+import { DockerizerLayout } from "@/components/Layout";
+import { FormSelect } from "@/components/ui/form-fields";
+import {
+  defaultValues,
+  generate,
+  nodeImageOptions,
+  schema,
+} from "@/dockerizers/nestjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+export default function Page() {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues,
+  });
+
+  return (
+    <DockerizerLayout title="Nest.js" form={form} generate={generate}>
+      <FormSelect
+        control={form.control}
+        name="nodeImage"
+        label="Node image"
+        options={nodeImageOptions}
+      />
+    </DockerizerLayout>
+  );
+}
