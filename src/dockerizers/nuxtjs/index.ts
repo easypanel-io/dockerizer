eaf624486d77e394d3709fb5ac8a7e69030f69fe
serban-alexandru <alexserb2009@yahoo.com>
2024-01-15 16:44:34 +0200

    nuxt js test

---
+++ src/dockerizers/nuxtjs/index.ts
@@ -0,0 +1,33 @@
+import { Files } from "@/lib/types";
+import { renderString } from "nunjucks";
+import { z } from "zod";
+import files from "./files.json";
+
+export const schema = z.object({
+  nodeImage: z.string().default("node:20"),
+  telemetry: z.boolean().default(false),
+  host: z.string().default("0.0.0.0"),
+  port: z.string().default("3000"),
+  // advanced
+  filePermissionsUser: z.string().default("nextjs"),
+  filePermissionsGroup: z.string().default("nodejs"),
+  alpinePackages: z.string().default("libc6-compat"),
+});
+
+export const nodeImageOptions = [
+  "node:16",
+  "node:16-alpine",
+  "node:18",
+  "node:18-alpine",
+  "node:20",
+  "node:20-alpine",
+];
+
+export const defaultValues = schema.parse({});
+
+export function generate(rawInput: z.infer<typeof schema>): Files {
+  const input = schema.parse(rawInput);
+  return {
+    Dockerfile: renderString(files["Dockerfile.njk"], input),
+  };
+}
---
+++ src/pages/nuxtjs/index.tsx
@@ -0,0 +1,59 @@
+import { DockerizerLayout } from "@/components/Layout";
+import { FormInput, FormSelect, FormSwitch } from "@/components/ui/form-fields";
+import {
+  defaultValues,
+  generate,
+  nodeImageOptions,
+  schema,
+} from "@/dockerizers/nuxtjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+export default function Page() {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues,
+  });
+
+  return (
+    <DockerizerLayout title="Nuxt.js" form={form} generate={generate}>
+      <FormSelect
+        control={form.control}
+        name="nodeImage"
+        label="Node image"
+        options={nodeImageOptions}
+      />
+      <FormSwitch
+        control={form.control}
+        name="telemetry"
+        label="Telemetry"
+        description="Check if you want Telemetry"
+      />
+      <FormInput
+        control={form.control}
+        name="filePermissionsUser"
+        label="File Permissions (User)"
+        inputProps={{
+          placeholder: "...",
+        }}
+      />
+      <FormInput
+        control={form.control}
+        name="filePermissionsGroup"
+        label="File Permissions (Group)"
+        inputProps={{
+          placeholder: "...",
+        }}
+      />
+      <FormInput
+        control={form.control}
+        name="alpinePackages"
+        label="Alpine packages"
+        inputProps={{
+          placeholder: "...",
+        }}
+      />
+    </DockerizerLayout>
+  );
+}
