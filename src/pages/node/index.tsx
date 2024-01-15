71364a45195207188f7218cdaacd246f5aceca17
serban-alexandru <alexserb2009@yahoo.com>
2024-01-15 16:37:20 +0200

    node & express test

---
+++ src/pages/node/index.tsx
@@ -0,0 +1,38 @@
+import { DockerizerLayout } from "@/components/Layout";
+import { FormInput, FormSelect } from "@/components/ui/form-fields";
+import {
+  defaultValues,
+  generate,
+  nodeImageOptions,
+  schema,
+} from "@/dockerizers/node";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+export default function Page() {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues,
+  });
+
+  return (
+    <DockerizerLayout title="Node.js" form={form} generate={generate}>
+      <FormSelect
+        control={form.control}
+        name="nodeImage"
+        label="Node image"
+        options={nodeImageOptions}
+      />
+      <FormInput
+        control={form.control}
+        name="port"
+        label="Port"
+        inputProps={{
+          type: "number",
+          placeholder: "3000",
+        }}
+      />
+    </DockerizerLayout>
+  );
+}
