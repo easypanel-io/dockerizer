7b4e74d8eb989a527d57edb7e58b7bde6b5028d9
Andrei Canta <deiucanta@gmail.com>
2024-01-31 15:55:21 +0100

    add ga tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,23 @@
 import { TooltipProvider } from "@/components/ui/tooltip";
 import "@/styles/globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <TooltipProvider>
-        <Component {...pageProps} />
-      </TooltipProvider>
-    </ThemeProvider>
+    <>
+      <GoogleAnalytics gaId="G-4JL0CD3631" />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <TooltipProvider>
+          <Component {...pageProps} />
+        </TooltipProvider>
+      </ThemeProvider>
+    </>
   );
 }
